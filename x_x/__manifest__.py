0c995b007bd92a54d24c4f022db6cd73ff8cc25b
Pyae Sone <pyaesonepaing06@gmail.com>
2026-06-11 21:28:16 +0630

    xxx

---
+++ x_x/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "x_x",
+    "author": "x_x",
+    "version": "18.0",
+    "category": "Accounting/Customizations",
+    "description": """Tasdfasdfasd,""",
+    "depends": ["base", "mail"],
+    "data": [
+        "views/views.xml",
+    ],
+    "icon": "/x_x/static/description/icon.png",
+    "license": "LGPL-3",
+    'auto_install'  :True,
+}
