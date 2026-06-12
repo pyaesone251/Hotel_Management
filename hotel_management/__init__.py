6017eb2fcc33ffa10a9723e8c203c3b7670bc577
Pyae Sone <pyaesonepaing06@gmail.com>
2026-06-12 22:39:12 +0630

    zzz

--- hotel_management/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Top-level package for hotel_management module."""
-
-from . import models
--- hotel_management/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Import all model files for hotel_management module."""
-
-from . import hotel
